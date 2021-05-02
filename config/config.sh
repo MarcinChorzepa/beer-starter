9f1d46bdf8bd444d0e37d2d6e75f79c7d1a1a719
Marcin Chorzepa <Marcin.Chorzepa@circlekeurope.com>
2021-05-02 11:22:29 +0200

    init

---
+++ config/config.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+declare -a EMOBLILITY_GIT=(
+git@github.com:circlekeurope/emobility-receipt-service.git
+git@github.com:circlekeurope/emobility-transaction-service.git
+git@github.com:circlekeurope/emobility-push-notification-service.git
+#git@github.com:circlekeurope/emobility-rfid-service.git
+)
+declare HOME_DIR="../emoblility-application"
---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+source config/config.sh
+for repo in "${EMOBLILITY_GIT[@]}"
+do
+  NAME=$repo
+  NAME=${NAME#*/}
+  NAME=${NAME%.*}
+  if [ -d $HOME_DIR/$NAME ]
+  then
+    echo "repository $NAME exists. (skipping)"
+  else
+    	echo "clonning repo $repo"
+    	git clone $repo $HOME_DIR/$NAME
+  fi
+#	GIT_IGNORE=$GIT_IGNORE"/"$NAME"\n"
+
+done
+#echo $GIT_IGNORE
+#echo -e "$GIT_IGNORE">.gitignore
